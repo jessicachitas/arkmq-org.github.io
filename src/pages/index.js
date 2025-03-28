d25426ecd1f7eaf87bad54aa68753daca7ef6a27
Jess Chitas <36636982+jessicachitas@users.noreply.github.com>
2025-03-28 09:55:47 +0000

    edit pages index
    Signed-off-by: Jess Chitas <36636982+jessicachitas@users.noreply.github.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,14 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 
 import Welcome from '@site/src/components/Welcome'
-import Install from '@site/src/components/Install'
-import Demo from '@site/src/components/Demo'
 import About from '@site/src/components/About'
+import Sponsors from '@site/src/components/Sponsors'
 
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`Welcome to ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <main>
         <Welcome /> 
