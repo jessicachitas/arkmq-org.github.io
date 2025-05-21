1b1b23d971f429d01c5c19eb14d5b3228a0ff2b8
Jess Chitas <36636982+jessicachitas@users.noreply.github.com>
2025-05-21 15:56:26 +0100

    clean up welcome section
    Signed-off-by: Jess Chitas <36636982+jessicachitas@users.noreply.github.com>

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,6 +1,3 @@
-import logo from "@site/static/img/logo-square.png";
-import icon from "@site/static/img/gravity-ui--persons.png";
-
 export default function Welcome() {
   return (
     <>
@@ -11,26 +8,17 @@ export default function Welcome() {
             A collection of container images that provide a way to deploy the
             Apache ActiveMQ Artemis Broker on Kubernetes
           </h2>
-          <ul className="list-buttons">
-            <li>
-              <button className="static-button" href="#">
-                <img className="icon" src={icon} alt="Button 1"></img>
-                Quick Start Guide here
-              </button>
-            </li>
-            <li>
-              <button className="static-button" href="#">
-                <img className="icon" src={icon} alt="Button 2"></img>
-                Contribute to the project
-              </button>
-            </li>
-            <li>
-              <button className="static-button" href="#">
-                <img className="icon" src={icon} alt="Button 3"></img>
-                View documentation here
-              </button>
-            </li>
-          </ul>
+          <div className="button-group">
+            <button className="static-button" href="#">
+              Quick Start Guide here
+            </button>
+            <button className="static-button" href="#">
+              Contribute to the project
+            </button>
+            <button className="static-button" href="#">
+              View documentation here
+            </button>
+          </div>
         </div>
       </div>
     </>
