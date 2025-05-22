467e8ae3aa5722f34922a433aa85c522dd55564f
Jess Chitas <36636982+jessicachitas@users.noreply.github.com>
2025-05-22 16:06:48 +0100

    add links to buttons on welcome section
    Signed-off-by: Jess Chitas <36636982+jessicachitas@users.noreply.github.com>

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,25 +1,38 @@
-import welcomeLogo from "@site/static/img/arkmq-logo.png"
-import welcomeLogoDark from "@site/static/img/arkmq-logo-darkmode.png"
+import { useHistory, useNavigate } from "react-router-dom";
+import welcomeLogo from "@site/static/img/arkmq-logo.png";
+import welcomeLogoDark from "@site/static/img/arkmq-logo-darkmode.png";
 
 export default function Welcome() {
+  const history = useHistory();
+
   return (
     <>
       <div className="welcome viewport">
         <div className="welcome-info">
-          <img className="welcome-logo" src={welcomeLogo}/>
-          <img className="welcome-logo-dark" src={welcomeLogoDark}/>
+          <img className="welcome-logo" src={welcomeLogo} />
+          <img className="welcome-logo-dark" src={welcomeLogoDark} />
           <h2 className="welcome-h2">
             A collection of container images that provide a way to deploy the
             Apache ActiveMQ Artemis Broker on Kubernetes
           </h2>
           <div className="button-group">
-            <button className="static-button" href="#">
+            <button
+              className="static-button"
+              onClick={() => history.push("/docs/getting-started/quick-start")}
+            >
               Quick Start Guide here
             </button>
-            <button className="static-button" href="#">
+            <button
+              className="static-button"
+              onClick={() => history.push("/docs/contribute")}
+            >
               Contribute to the project
             </button>
-            <button className="static-button" href="#">
+
+            <button
+              className="static-button"
+              onClick={() => history.push("/docs/getting-started")}
+            >
               View documentation here
             </button>
           </div>
