3c1a8acc4726df3f913b44765ab8688b11a4372c
Jess Chitas <36636982+jessicachitas@users.noreply.github.com>
2025-05-26 15:43:54 +0100

    add working carousel for sponsors section with placeholders for the time being
    Signed-off-by: Jess Chitas <36636982+jessicachitas@users.noreply.github.com>

--- src/components/Sponsors/index.js
+++ src/components/Sponsors/index.js
@@ -1,23 +1,47 @@
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper/modules";
+
+import Cloud from "@site/static/img/cloud-arrow-up-solid.png";
+import Kubernetes from "@site/static/img/box-open-solid.png";
+import Operator from "@site/static/img/user-solid.png";
+import Example from "@site/static/img/copy-solid.png";
+import CI from "@site/static/img/arrow-right-solid.png";
+
 export default function Sponsors() {
-    return (
-      <>
-        {/* ----------------------------- Install Section ---------------------------- */}
-        <div className="install viewport blue-theme" id="install">
-          <div className="install-info">
-            <h1 className="install-h1">How to Install header</h1>
-            <h2 className="install-h2">More info header</h2>
-  
-            {/* ----------- Modular Section (copy and paste as many as needed) ---------- */}
-            <h3 className="install-h3">Distro:</h3>
-            <p className="install-code">
-              If being installed via command line, command goes here
-            </p>
-            {/* -------------------------------------------------------------------------- */}
-              <button className="static-button install-button" role="button" href="#">
-                More install methods here
-              </button>
-          </div>
+  return (
+    <>
+      {/* ----------------------------- Install Section ---------------------------- */}
+      <div className="sponsor viewport blue-theme" id="sponsor">
+      
+        <div className="sponsor-carousel">
+          <h1 className="sponsor-title">Thank you to our Sponsors!</h1>
+          <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
+            <SwiperSlide>
+              <img src={Cloud} />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={Kubernetes} />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={Operator} />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={Example} />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={CI} />
+            </SwiperSlide>
+          </Swiper>
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
+}
