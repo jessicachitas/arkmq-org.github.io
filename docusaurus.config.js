b22e57481d099dbaced76327ac5aa3f899f8fa6b
Jess Chitas <36636982+jessicachitas@users.noreply.github.com>
2025-05-26 11:19:15 +0100

    update config
    Signed-off-by: Jess Chitas <36636982+jessicachitas@users.noreply.github.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
 
   // Set the production url of your site here
   url: "https://jessicachitas.github.io",
-  baseUrl: "/arkmq.github.io",
+  baseUrl: "/arkmq-org.github.io",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
