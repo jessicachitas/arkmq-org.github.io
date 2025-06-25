b15069677bb969ca8f2ce5b9e471da0ccf366e58
Jess Chitas <36636982+jessicachitas@users.noreply.github.com>
2025-06-25 11:32:17 +0100

    fix some styles
    Signed-off-by: Jess Chitas <36636982+jessicachitas@users.noreply.github.com>

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,39 +1,41 @@
 export default function About() {
   return (
-    <div className="show-for-mobile about about-viewport blue-theme" id="about">
-      <div className="about-grid">
-                    <div className="about-section-1">
-                      <h1 className="about-header">Cloud Orientated</h1>
-                      <p className="about-text" role="paragraph">
-                        The ability to quickly deploy and run the broker as a container
-                        image
-                      </p>
-                    </div>
-                    <div className="about-section-2">
-                      <h1 className="about-header">Kubernetes</h1>
-                      <p className="about-text" role="paragraph">
-                        The ability to run a Kubernetes native container on Kubernetes
-                      </p>
-                    </div>
-                    <div className="about-section-3">
-                      <h1 className="about-header">Operator</h1>
-                      <p className="about-text" role="paragraph">
-                        The operator based solution to manage the Kubernetes deployment
-                      </p>
-                    </div>
-                    <div className="about-section-4">
-                      <h1 className="about-header">Examples</h1>
-                      <p className="about-text" role="paragraph">
-                        A suite of examples to demonstrate each feature
-                      </p>
-                    </div>
-                    <div className="about-section-5">
-                      <h1 className="about-header">CI/CD</h1>
-                      <p className="about-text" role="paragraph">
-                        Continuous intergration and deployment for quick development
-                      </p>
-                    </div>
-                  </div>
+    <div className="show-for-mobile">
+      <div className="about about-viewport blue-theme" id="about">
+        <div className="about-grid">
+          <div className="about-section-1">
+            <h1 className="about-header">Cloud Orientated</h1>
+            <p className="about-text" role="paragraph">
+              The ability to quickly deploy and run the broker as a container
+              image
+            </p>
+          </div>
+          <div className="about-section-2">
+            <h1 className="about-header">Kubernetes</h1>
+            <p className="about-text" role="paragraph">
+              The ability to run a Kubernetes native container on Kubernetes
+            </p>
+          </div>
+          <div className="about-section-3">
+            <h1 className="about-header">Operator</h1>
+            <p className="about-text" role="paragraph">
+              The operator based solution to manage the Kubernetes deployment
+            </p>
+          </div>
+          <div className="about-section-4">
+            <h1 className="about-header">Examples</h1>
+            <p className="about-text" role="paragraph">
+              A suite of examples to demonstrate each feature
+            </p>
+          </div>
+          <div className="about-section-5">
+            <h1 className="about-header">CI/CD</h1>
+            <p className="about-text" role="paragraph">
+              Continuous intergration and deployment for quick development
+            </p>
+          </div>
         </div>
+      </div>
+    </div>
   );
 }
